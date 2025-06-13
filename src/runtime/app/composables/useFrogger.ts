79f1726191b27fe144ce88e9af1dcd2e975c3d81
Evan Kelly <masterevank@gmail.com>
2025-06-13 15:32:00 +1000

    chore: add defineNitroPlugin imports and pass client frogger options (#82)

--- src/runtime/app/composables/useFrogger.ts
+++ src/runtime/app/composables/useFrogger.ts
@@ -6,16 +6,16 @@ import type { IFroggerLogger } from '../../logger/types';
 
 import { APP_MOUNTED_STATE_KEY } from '../../shared/types/module-options';
 
-
+import type { ClientLoggerOptions } from '../../logger/client/types';
 
 /**
  * Composable to access the Frogger logger from any component
  */
-export function useFrogger(): IFroggerLogger {
+export function useFrogger(options: ClientLoggerOptions): IFroggerLogger {
     //@ts-ignore
     const hasMounted = useState<boolean>(APP_MOUNTED_STATE_KEY, () => false);
 
-    const logger = new ClientFrogger(computed(() => hasMounted.value));
+    const logger = new ClientFrogger(computed(() => hasMounted.value), options);
 
     return logger;
 }
